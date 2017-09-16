4c0118e384101bb429c858deb6306083bd8dad01
Nathaniel Daines <nated@koorong.com.au>
2017-09-16 11:11:16 +1000

    Finished Profile Img + Links to Right

--- dist/header.php
+++ dist/header.php
@@ -29,6 +29,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 			background-repeat: no-repeat;
 			background-position: center;
 		}
+
+		<?php 
+			if (is_user_logged_in()) {
+				echo ".navbar { top: 32px !important }";
+			}
+		?>
   </style>
 </head>
 
@@ -42,7 +48,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content',
 		'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-light bg-light">
+		<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container">
@@ -76,7 +82,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						'theme_location'  => 'primary',
 						'container_class' => 'collapse navbar-collapse',
 						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav',
+						'menu_class'      => 'navbar-nav ml-auto',
 						'fallback_cb'     => '',
 						'menu_id'         => 'main-menu',
 						'walker'          => new WP_Bootstrap_Navwalker(),
