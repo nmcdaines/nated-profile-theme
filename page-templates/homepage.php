843574e9cb35936fab5d75eb9bd81cd1700983b0
Nathaniel Daines <nated@koorong.com.au>
2017-09-20 15:12:11 +1000

    Updated Theme includes Skills Container (in development)

---
+++ page-templates/homepage.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * Has linked scripts for 
+ *
+ * @package understrap
+ */
+
+get_header();
+
+while ( have_posts() ) : the_post();
+	get_template_part( 'loop-templates/content', 'empty' );
+endwhile;
+
+
+/**
+ * Custom Scripts go here (outside the quotes)
+ */
+
+echo '<script src="' . get_template_directory_uri() . '/src/plugins/skills/main.js' . '"></script>';
+echo '<link rel="stylesheet" type="text/css" href="' . get_template_directory_uri() . '/src/plugins/skills/main.css' . '" />';
+
+
+echo '<script src="' . get_template_directory_uri() . '/src/plugins/experience/main.js' . '"></script>';
+echo '<link rel="stylesheet" type="text/css" href="' . get_template_directory_uri() . '/src/plugins/experience/main.css' . '" />';
+
+
+get_footer();
